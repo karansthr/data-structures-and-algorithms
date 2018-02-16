6f64e1284c84b3ef1d7a04512580f09e82a4c39d
Mohit Solanki <mohitsolanki619@gmail.com>
2018-02-16 19:16:42 +0530

    Update shellsort.py

--- algorithms/sorting/shellsort.py
+++ algorithms/sorting/shellsort.py
@@ -1,9 +1,25 @@
-def shell_sort(arr):
-    pass
+def shell_sort(array):
+    """
+    Shell sort algorithm improves upon the insertion sort by first
+    sorting the elements which are certain interval apart and then
+    running insertion sort, thereby reducing number of shifting of
+    elements.
+    """
+    gap = len(array) // 2
+    while gap > 0:
+        for i in range(gap, len(array)):
+            key = array[i]
+            j = i
+            while j >= gap and key < array[j - gap]:
+                array[j] = array[j - gap]
+                j = j - gap
+            array[j] = key
+        gap = gap // 2
+    return array
 
 
 if __name__ == '__main__':
-    arr = list(map(int, input('Enter space seperated numbers:').split()))
+    arr = list(map(int, input('Enter space seperated numbers: ').split()))
     arr = shell_sort(arr)
     print('After sorting')
     print(*arr)
