42c826c4bdfbbecc2abf28dce8bc78ae1cb68182
karansthr <karansthr97@gmail.com>
2018-02-28 22:59:52 +0530

    added some methods on linked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+
+
 class LinkedList:
     class Node:
         def __init__(self, val=None):
@@ -8,69 +11,58 @@ class LinkedList:
             return str(self.val)
 
     def __init__(self):
-        self.head = self.Node()
+        self.head = None
+        self.tail = None
+
+    def __str__(self):
+        return  '< ' + ', '.join([str(i) for i in self.traverser()]) + ' >'
 
     def __len__(self):
         count = 0
-        traverser = self.head
-        while not traverser:
-            traverser = traverser.next
+        runner = self.head
+        while runner:
+            runner = runner.next
             count = count + 1
         return count
 
-    def printlist(self):
-        traverser = self.head
-        while traverser:
-            print(traverser.val, end=", ")
-            traverser = traverser.next
+    def traverser(self):
+        runner = self.head
+        while runner.next:
+            yield runner.val
+            runner = runner.next
 
-    def last(self):
-        previous = traverser = self.head
-        while traverser:
-            previous = traverser
-            traverser = traverser.next
-        return previous
+    def printlist(self):
+        for i in self.traverser():
+            print(i, end=", ")
 
     def append(self, val):
-        if not len(self):
+        if self.head is None:
             self.head = self.Node(val)
+            self.last = self.head
         else:
-            _last = self.last()
+            last = self.last
             node = self.Node(val)
-            _last.next = node
+            last.next = node
+            self.last = node
 
     def extend(self, iterable):
-        pass
+        for i in iterable:
+            self.append(i)
 
     def count(self, key):
-        pass
-
-    def clear(self):
-        pass
+        counter = 0
+        for i in self.traverser():
+            if i == key:
+                counter += 1
+        return counter
 
     def reverse(self):
-        pass
-
-    def remove(self, key):
-        pass
-
-    def remove_all(self, key):
-        pass
-
-    def merge_sort(self):
-        pass
-
-    def quick_sort(self):
-        pass
-
-    def insertion_sort(self):
-        pass
-
-    def selection_sort(self):
-        pass
-
-    def get_head(self):
-        pass
-
-    def get_tail(self):
-        pass
+        alpha = self.head
+        beta = alpha.next
+        alpha.next = None
+        while beta:
+            gamma = beta.next
+            beta.next = alpha
+            alpha = beta
+            beta = gamma
+        self.head = alpha
